054cea1e24c76cd5bd1345b8495dd7cb596f8103
Arup117 <arupmadhu@live.com>
2013-09-21 22:45:04 +0530

    fresh commit

--- DataStructure/common.h
+++ DataStructure/common.h
@@ -1,5 +1,22 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
-  #define DELETE(p)         if(p){delete p;p = NULL;} 
-  #define ARRAY_SIZE(p)     (sizeof(p)/sizeof(p[0]))
+
+#if !defined(SUCCESS) && !defined(FAILED)
+  #define SUCCESS 0
+  #define FAILED  1
+#endif
+
+#if !defined(SIZE) && !defined(MIN) && !defined(MAX)
+  #define     SIZE(arr)             (sizeof(arr)/sizeof(arr[0]))
+  #define     MIN(a,b)              (((a)<(b))?(a):(b))
+  #define     MAX(a,b)              (((a)>(b))?(a):(b))
+#endif
+
+#if !defined(BREAKIF) && !defined(BREAKIFNULL)
+  #define BREAKIF(nRetVal) if (SUCCESS != nRetVal) {break;}
+  #define BREAKIFNULL(p,nRetVal) if( NULL == (p)){nRetVal = FAILED;break;}
+#endif
+
+  typedef unsigned int uint;
+
 #endif
