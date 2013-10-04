33eb9c13eaa7320be2b893d2349a77aa5327d5bf
Arup117 <arupmadhu@live.com>
2013-10-04 19:57:51 +0530

    Made some changes

--- Algorithm/Greedy/MakingChange.cpp
+++ Algorithm/Greedy/MakingChange.cpp
@@ -3,9 +3,57 @@
 
 using std::cout;
 using std::cin;
+using std::endl;
 using std::vector;
+
+typedef struct __Data{
+      int           nTotalVal;
+      int           nNumCoins;
+}Data;
+
+class MakeChangeInterface{
+  public:
+    virtual void doMakeChange() = 0;
+};
+
+class GreedyApproachMakeChange : public MakeChangeInterface{
+  public:
+    virtual void doMakeChange(){
+    
+    }
+};
+
+class DynamicApproachMakeChange : public MakeChangeInterface{
+  public:
+    virtual void doMakeChange(){
+      //TODO 
+    }
+};
+
+class MakeChange{
+    MakeChangeInterface* pInterface;
+    MakeChange(MakeChangeInterface* pIF)
+    {
+       this->pInterface = pIF; 
+    }
+    void doMakeChange()
+    {
+      if(this->pInterface)
+        this->pInterface->doMakeChange();
+      else
+        cout<<"You forgot to set the Interface!!"<<endl;
+    }
+};
 
 int 
 main(int argc,char** argv){
+  Data* pData = new Data();
+  cout<<"What is the Total amaout?"<<endl; 
+  cin>>pData->nTotalVal;
+  
+  cout<<"How many coins are there?"<<endl; 
+  cin>>pData->nNumCoins;
+  
+  delete pData;
   return 0;
 }
