e32abe90d2cb867e683b43c0299732c74663d1e9
Arup117 <arupmadhu@live.com>
2013-10-05 08:33:40 +0530

    getting compilation error

--- Algorithm/Greedy/MakingChange.cpp
+++ Algorithm/Greedy/MakingChange.cpp
@@ -1,5 +1,6 @@
 #include "iostream"
 #include "vector"
+#include <stdlib.h>
 
 using std::cout;
 using std::cin;
@@ -9,51 +10,82 @@ using std::vector;
 typedef struct __Data{
       int           nTotalVal;
       int           nNumCoins;
+      int*          pCoins;
 }Data;
 
 class MakeChangeInterface{
   public:
-    virtual void doMakeChange() = 0;
+    virtual void doMakeChange(Data& data) = 0;
+
+    int compare(const void* num1,const void* num2){
+        int nResult = 0;
+        return nResult;
+    }
 };
 
 class GreedyApproachMakeChange : public MakeChangeInterface{
   public:
-    virtual void doMakeChange(){
-    
+    virtual void doMakeChange(Data& data){
+       qsort(data.pCoins,data.nNumCoins,sizeof(int),this->compare);     
     }
 };
 
 class DynamicApproachMakeChange : public MakeChangeInterface{
   public:
-    virtual void doMakeChange(){
-      //TODO 
+    virtual void doMakeChange(Data& data){
     }
 };
 
 class MakeChange{
+  private:
     MakeChangeInterface* pInterface;
+    Data* pData;
+  public:
     MakeChange(MakeChangeInterface* pIF)
     {
-       this->pInterface = pIF; 
+       this->pInterface = pIF;
+       this->pData      = new Data(); 
     }
-    void doMakeChange()
+    
+    void doMakeChange(void)
     {
       if(this->pInterface)
-        this->pInterface->doMakeChange();
+        this->pInterface->doMakeChange(*pData);
       else
         cout<<"You forgot to set the Interface!!"<<endl;
     }
+    
+    void fillData(void)
+    {
+        cout<<"What is the Total amaout?"<<endl; 
+        cin>>pData->nTotalVal;
+  
+        cout<<"How many coins are there?"<<endl; 
+        cin>>pData->nNumCoins;
+
+        pData->pCoins = new int[pData->nNumCoins];
+        cout<<"Enter the coin values:"<<endl; 
+
+        for (int i = 0; i < pData->nNumCoins; i++) 
+        {
+            cin>>(pData->pCoins)[i];
+        }
+    }
+
+    ~MakeChange()
+    {
+        if(this->pData)
+          delete [] pData;
+        if(this->pInterface)
+          this->pInterface = NULL;
+    }
 };
+
 
 int 
 main(int argc,char** argv){
-  Data* pData = new Data();
-  cout<<"What is the Total amaout?"<<endl; 
-  cin>>pData->nTotalVal;
-  
-  cout<<"How many coins are there?"<<endl; 
-  cin>>pData->nNumCoins;
-  
-  delete pData;
+  MakeChange* pMakeChange = new MakeChange(new GreedyApproachMakeChange());
+  pMakeChange->fillData();
+  delete pMakeChange;
   return 0;
 }
