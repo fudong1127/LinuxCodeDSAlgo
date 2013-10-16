51faf9a4423768c704772cb36340d50f9d236533
Arup117 <arupmadhu@live.com>
2013-10-16 10:08:57 +0530

    changed design

--- DataStructure/BSTree/getBSTreeSize.cpp
+++ DataStructure/BSTree/getBSTreeSize.cpp
@@ -1,13 +1,11 @@
 #include "arupBSTree.h"
 
 unsigned int 
-getBSTreeSize(arupBSTreeNode* root){
-   if(!root)
-       return 0;
-   else{
-        int ht_left_sub_tree  = getBSTreeSize(root->left);
-        int ht_right_sub_tree = getBSTreeSize(root->right);
-
-        return (1 + ht_left_sub_tree + ht_right_sub_tree);
-   }
+getBSTreeSize(arupBSTreeNode* root)
+{
+  if(root)
+  {
+      return (1 + getBSTreeSize(root->left) + getBSTreeSize(root->right));
+  }
+  return 0;
 }
